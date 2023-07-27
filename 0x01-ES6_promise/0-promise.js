cf7fe21fcde6b8fc258d5190957b7cef0bf231b4
bdot100 <bidemiokunade@gmail.com>
2023-07-27 17:01:11 +0100

    Project directory set up and first taks done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,6 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    // eslint-disable-next-line because no-constant-condition
   if (true) {
     resolve();
   } else {

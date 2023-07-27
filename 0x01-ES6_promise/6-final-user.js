632880daf4e9ff062941c2073273b55220e5ccfe
bdot100 <bidemiokunade@gmail.com>
2023-07-27 18:27:51 +0100

    Try, catch and finally block done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,25 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    const signUp = signUpUser(fileName, lastName);
+    const photo = uploadPhoto(fileName);
+
+    return Promise.allSettled([signUp, photo]).then((vals) => {
+        const resultArr = [];
+        vals.forEach((val) => {
+            if (val.status === 'fulfilled') {
+                resultArr.push({
+                    status: val.status,
+                    value: val.value
+                });
+            } else {
+                resultArr.push({
+                    status: val.status,
+                    value: `Error: ${val.reason}`,
+                });
+            }
+        });
+        return resultArr;
+    });
+}

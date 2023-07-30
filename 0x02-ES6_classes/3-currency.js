19a71829275545235f8b8836cc61a65b04ff700e
bdot100 <bidemiokunade@gmail.com>
2023-07-30 22:17:26 +0100

    Currency class implemented

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+
+    // code getter
+    get code() {
+        return this._code;
+    }
+
+    //code setter
+    set code(code) {
+        if (typeof code === 'string') {
+            this._code = code;
+        } else {
+            throw new TypeError('Code must be a string!')
+        }
+    }
+
+    // name getter
+    get name() {
+        return this._name;
+    }
+
+    // name setter
+    set name(name) {
+        if (typeof name === 'string') {
+            this._name = name
+        } else {
+            throw new TypeError('Name must be a string!')
+        }
+    }
+
+    displayFullCurrency() {
+        return `${this.name} (${this.code})`;
+    }
+}

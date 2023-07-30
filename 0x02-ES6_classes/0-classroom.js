326115a350a421d869dee398c3f34faadb38da24
bdot100 <bidemiokunade@gmail.com>
2023-07-30 22:05:26 +0100

    Holberton course class implemented

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -3,15 +3,13 @@ export default class ClassRoom {
     this._maxStudentsSize = maxStudentsSize;
   }
 
-  set _maxStudentsSize(newStudentsSize) {
-    if (typeof maxStudentsSize === 'number') {
+  set maxStudentsSize(newStudentsSize) {
+    if (typeof newStudentsSize === 'number') {
       this._maxStudentsSize = newStudentsSize;
-    } else {
-      throw new TypeError('Size of students must be a number!');
     }
   }
 
-  get _maxStudentsSize() {
+  get maxStudentsSize() {
     return this._maxStudentsSize;
   }
 }

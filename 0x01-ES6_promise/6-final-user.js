48d0cbddc566008a1772f86239275b57c3bdea24
bdot100 <bidemiokunade@gmail.com>
2023-07-30 19:31:57 +0100

    Try, catch and finally block done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(fileName, lastName);
+  const signUp = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
   return Promise.allSettled([signUp, photo]).then((vals) => {

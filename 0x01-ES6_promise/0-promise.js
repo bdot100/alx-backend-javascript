0a69ed6864fb6e7b6102c7e8330db0866b696aea
bdot100 <bidemiokunade@gmail.com>
2023-07-30 17:16:46 +0100

    Try, catch and finally block done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     // eslint-disable-next-line because no-constant-condition
-  if (true) {
-    resolve();
-  } else {
-    reject();
+    if (true) {
+      resolve();
+    } else {
+      reject();
   }
   });
 }

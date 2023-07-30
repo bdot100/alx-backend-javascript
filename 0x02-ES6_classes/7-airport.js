45ba84fd853dea7474f11451182d203f3780f43b
bdot100 <bidemiokunade@gmail.com>
2023-07-30 23:26:01 +0100

    Airport class implemented

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,3 +1,39 @@
 export default class Airport {
-    
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // get name
+  get name() {
+    return this._name;
+  }
+
+  // set name
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Type must be string');
+    }
+  }
+
+  // get code
+  get code() {
+    return this._code;
+  }
+
+  // set code
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Type must be string');
+    }
+  }
+
+  // Default string description
+  toString() {
+    return this._code;
+  }
 }

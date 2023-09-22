3372408d464cf54a4483c35fb7133ffdc8f279a8
bdot100 <bidemiokunade@gmail.com>
2023-09-22 10:31:55 +0100

    Small HTTP Server Using Node module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+// Create an HTTP server
+const app = http.createServer((req, res) => {
+  // Set the status code
+  res.statusCode = 200;
+
+  // Set the response headers
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Respond with "Hello Holberton School!" for any endpoint
+  res.end('Hello Holberton School!\n');
+});
+
+// Listen on port 1245
+const port = 1245;
+const hostname = '127.0.0.1';
+app.listen(port, hostname, () => {
+});
+
+// Export the app for external use
+module.exports = app;

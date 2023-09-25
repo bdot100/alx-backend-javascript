5f00b4ffb5d125f88d3ef8c63e1d174e588c9c88
bdot100 <bidemiokunade@gmail.com>
2023-09-25 17:36:00 +0100

    Basic test with mocha and node assertion

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,4 +11,29 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(0, 0), 0);
     assert.strictEqual(calculateNumber(-1.6, -1.7), -4);
   });
+
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(1, 2);
+    assert.strictEqual(res, 3);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(1.4, 2.2);
+    assert.strictEqual(res, 3);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(1.6, 2.7);
+    assert.strictEqual(res, 5);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(-1.6, -1.7);
+    assert.strictEqual(res, -4);
+  });
+  it(`checking if numbers round`, function() {
+    const res = calculateNumber(-1.4, -1.3);
+  assert.strictEqual(res, -2);
+  });
 });

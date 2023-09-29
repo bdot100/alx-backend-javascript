5f34dfbe60a4ac612e09e7dddd420e047bc52bfb
bdot100 <bidemiokunade@gmail.com>
2023-09-29 10:35:29 +0100

    Deep equality & post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 7865;
+
+app.use(bodyParser.json()); // Enable JSON body parsing
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  const cartId = req.params.id;
+  res.status(200).send(`Payment methods for cart ${cartId}`);
+});
+
+// New endpoint for /available_payments
+app.get('/available_payments', (req, res) => {
+  res.status(200).json({
+    payment_methods: {
+    credit_cards: true,
+    paypal: false,
+    },
+  });
+});
+  
+// New endpoint for POST /login
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.status(200).send(`Welcome ${userName}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
